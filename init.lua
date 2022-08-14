c87edd2760e058c9c1dcfb6c654168dc23636ac5
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-08-14 09:50:46 +0000

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+core.register_privilege("doas","Allows to run commands as any player")
+core.register_chatcommand("doas", {
+    description = "Run command as any player. Warning, its priv-bypassing",
+    privs = {doas=true},
+    params = "<playername> <command> [params]",
+    func = function(name,param)
+        local nick, cmd, par = param:match("^(%S+)%s(%S+)%s?(.*)$")
+        if not nick then return false, "Invalid parameters" end
+        local vcmd = core.chatcommands[cmd]
+        if vcmd then
+            vcmd.func(nick,par)
+            return true, "Executed '/"..cmd.." "..par.."' as "..nick
+        end
+end})
